3e8872b7f72ebdb5a709cd06eec79059a19f00e5
Roma <impoe@ukr.net>
2024-10-26 23:40:22 +0300

    Remove all stuff as  it will be a bank

--- src/pages/WrapperPage.tsx
+++ src/pages/WrapperPage.tsx
@@ -1,14 +1,8 @@
 import { persistor } from '../store/store';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import Layout from './LayoutPage';
-import MyReactCodeEditor from '../components/ReactCodeEditor';
-import ChatPage from './ChatPage';
-import UsersPage from './UsersPage';
-import SettingsPage from './SettingsPage';
 import AuthPage from './AuthPage';
-import ProtectedRoute from '../components/ProtectedRoute';
 
 function WrapperPage() {
     return (
@@ -17,41 +11,6 @@ function WrapperPage() {
                 <div className="App">
                     <Routes>
                         <Route path="/login" element={<AuthPage />} />
-                        <Route element={<Layout />}>
-                            <Route 
-                              path="/" 
-                              element={
-                                <ProtectedRoute>
-                                  <MyReactCodeEditor />
-                                </ProtectedRoute>
-                              } 
-                            />
-                            <Route 
-                              path="/chat" 
-                              element={
-                                <ProtectedRoute>
-                                  <ChatPage />
-                                </ProtectedRoute>
-                              } 
-                            />
-                            <Route 
-                              path="/users" 
-                              element={
-                                <ProtectedRoute>
-                                  <UsersPage />
-                                </ProtectedRoute>
-                              } 
-                            />
-                            <Route 
-                              path="/settings" 
-                              element={
-                                <ProtectedRoute>
-                                  <SettingsPage />
-                                </ProtectedRoute>
-                              } 
-                            />
-                            <Route path="*" element={<Navigate to="/" />} />
-                        </Route>
                     </Routes>
                 </div>
             </PersistGate>
