e9b5988a4a0a48be3b6254030dc9e7b889dc80f9
Roma <impoe@ukr.net>
2024-10-27 00:49:14 +0300

    stuff

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
-// export const ULR = 'http://18.206.155.178:8080'
-export const ULR = 'http://localhost:8080'
+export const ULR = 'http://18.206.155.178:8080'
+// export const ULR = 'http://localhost:8080'
